aee7b3b7770f49b86bcc8926d125483e6125c566
erin williams <erin.cwilliams0@gmail.com>
2025-08-13 12:57:46 -0500

    Add project files.

---
+++ TheSampleApi/Program.cs
@@ -0,0 +1,18 @@
+using TheSampleApi.Endpoints;
+using TheSampleApi.Startup;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.AddDependencies();
+
+var app = builder.Build();
+
+app.UseOpenApi();
+
+app.UseHttpsRedirection();
+
+app.AddRootEndpoints();
+
+app.Run();
---
+++ TheSampleApi/Startup/OpenApiConfig.cs
@@ -0,0 +1,28 @@
+﻿using Scalar.AspNetCore;
+
+namespace TheSampleApi.Startup
+{
+    public static class OpenApiConfig
+    {
+        public static void AddOpenApiServices(this IServiceCollection services)
+        {
+            services.AddOpenApi();
+        }
+
+        public static void UseOpenApi(this WebApplication app)
+        {
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+                app.MapScalarApiReference(options =>
+                {
+                    options.Title = "The Sample API";
+                    options.Theme = ScalarTheme.Saturn;
+                    options.Layout = ScalarLayout.Modern;
+                    options.HideClientButton = true;
+                });
+            }
+        }
+    }
+}
