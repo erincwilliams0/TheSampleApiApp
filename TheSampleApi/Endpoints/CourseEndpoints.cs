a3c69470f793635d006f17f953be0e9fa55a3cae
erin williams <erin.cwilliams0@gmail.com>
2025-08-13 18:05:38 -0500

    feat: Added two course endpoints one that gets all courses and another that grabs a single course by its id

---
+++ TheSampleApi/Endpoints/CourseEndpoints.cs
@@ -0,0 +1,30 @@
+﻿using TheSampleApi.Data;
+
+namespace TheSampleApi.Endpoints
+{
+    public static class CourseEndpoints
+    {
+        public static void AddCourseEndpoints(this WebApplication app)
+        {
+            app.MapGet("/courses", LoadAllCourses);
+            app.MapGet("/courses/{id}", LoadCourseById);
+        }
+
+        private static IResult LoadAllCourses(CourseData data)
+        {
+            return Results.Ok(data.Courses);
+        }
+
+        private static IResult LoadCourseById(CourseData data, int id)
+        {
+            var output = data.Courses.SingleOrDefault(x => x.Id == id);
+
+            if (output is null)
+            {
+                return Results.NotFound();
+            }
+
+            return Results.Ok(output);
+        }
+    }
+}
