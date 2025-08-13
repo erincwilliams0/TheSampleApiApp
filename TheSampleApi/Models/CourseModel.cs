14cde61d6e2545bdfe83ca79d37efc2c5a80d1c4
erin williams <erin.cwilliams0@gmail.com>
2025-08-13 18:05:58 -0500

    feat: changed CourseLengthInHours to a double to avoid errors with data

--- TheSampleApi/Models/CourseModel.cs
+++ TheSampleApi/Models/CourseModel.cs
@@ -8,7 +8,7 @@ public class CourseModel
     public required string CourseType { get; set; }
     public required string CourseName { get; set; }
     public int CourseLessonCount { get; set; }
-    public int CourseLengthInHours { get; set; }
+    public double CourseLengthInHours { get; set; }
     public required string ShortDescription { get; set; }
     public required string CourseImage { get; set; }
     public int PriceInUSD { get; set; }
