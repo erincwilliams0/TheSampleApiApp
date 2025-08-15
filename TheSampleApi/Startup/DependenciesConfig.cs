828fee78099a362b744f6d755f6ea16ddf65d50b
erin williams <erin.cwilliams0@gmail.com>
2025-08-14 19:24:29 -0500

    feat: added cors services and all health checks in dependency injection

--- TheSampleApi/Startup/DependenciesConfig.cs
+++ TheSampleApi/Startup/DependenciesConfig.cs
@@ -7,6 +7,8 @@ public static class DependenciesConfig
     public static void AddDependencies(this WebApplicationBuilder builder)
     {
         builder.Services.AddOpenApiServices();
+        builder.Services.AddCorsServices();
+        builder.Services.AddAllHealthChecks();
         builder.Services.AddTransient<CourseData>();
     }
 }
