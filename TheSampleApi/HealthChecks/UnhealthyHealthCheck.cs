aef3347516a7e689c512b674b5461c913295e256
erin williams <erin.cwilliams0@gmail.com>
2025-08-14 19:22:58 -0500

    feat: added simulated api health checks for healthy, degraded, unhealthy, and random api health checks

---
+++ TheSampleApi/HealthChecks/UnhealthyHealthCheck.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace TheSampleApi.HealthChecks
+{
+    public class UnhealthyHealthCheck : IHealthCheck
+    {
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            return Task.FromResult(
+                HealthCheckResult.Unhealthy("This is a test unhealthy service."));
+        }
+    }
+}
